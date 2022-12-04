deda1be906eff67d47a0956074868879824fe8a4
axel-nils-chalmers <axelnils@student.chalmers.se>
2022-12-04 15:43:51 +0100

    saving data to file

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from machine import Pin, Timer, I2C
 import struct
-from time import sleep
+from time import sleep, localtime
 
 # Constants 
 MS_ADDR = 0x36
@@ -8,7 +8,8 @@ MS_TEMP_BASE = 0x00
 MS_TEMP_OFFSET = 0x04
 MS_TOUCH_BASE = 0x0F
 MS_TOUCH_OFFSET = 0x10
-
+DATA_FILENAME = "data.txt"
+MEASUREMENT_DELAY = 60 * 1000
 
 # Function definitions
 def read_sensor(base, offset, nbytes):
@@ -22,8 +23,8 @@ def read_sensor(base, offset, nbytes):
 
 
 def get_temp():
-    buf = read_sensor(MS_TEMP_BASE, MS_TEMP_OFFSET, 4)
-    # buf[0] = buf[0] & 0x3F
+    buf = bytearray(read_sensor(MS_TEMP_BASE, MS_TEMP_OFFSET, 4))
+    buf[0] = buf[0] & 0x3F
     ret = struct.unpack(">I", buf)[0]
     return (0.00001525878 * ret)
 
@@ -42,8 +43,15 @@ def tick(timer):
     t = get_temp()
     ledgreen.value(t > 28)
     c_t = int(t - 5)
-    
+    write_value(m)
     print(f"Fuktighet: {c_m}%, Temp {c_t} C")
+
+
+def write_value(x):
+    file1 = open(DATA_FILENAME, "a")
+    l = localtime()
+    file1.write(f"{x} ({l[2]}/{l[1]}/{l[0]} {l[3]}:{l[4]})\n")
+    file1.close()
 
 
 # Setup
@@ -53,13 +61,14 @@ ledgreen = Pin(11, Pin.OUT)
 i2c = I2C(0, scl=machine.Pin(17), sda=machine.Pin(16))
 
 tim = Timer()
-tim.init(period=10000, callback=tick)
+tim.init(period=MEASUREMENT_DELAY, callback=tick)
 
 
 # Program start
 
 ledgreen.off()
 ledred.off()
+
 
 devices = i2c.scan()
 if devices:
