3b99e6b8ba82b82a4236f4c6945cb16b1710f4f6
axel-nils-chalmers <axelnils@student.chalmers.se>
2022-12-11 10:53:54 +0100

    data file handeling

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from machine import Pin, Timer, I2C
+from machine import Pin, Timer, I2C, RTC
 import struct
 from time import sleep, localtime
 
@@ -8,8 +8,12 @@ MS_TEMP_BASE = 0x00
 MS_TEMP_OFFSET = 0x04
 MS_TOUCH_BASE = 0x0F
 MS_TOUCH_OFFSET = 0x10
-DATA_FILENAME = "data.txt"
-MEASUREMENT_DELAY = 60 * 1000
+
+DATA_FILE = "data.txt"
+TIME_FILE = "safe_time.txt"
+MEASUREMENT_DELAY = 3600 * 1000
+START_TIME = (2022, 12, 11, 7, 10, 50, 0, 0)
+
 
 # Function definitions
 def read_sensor(base, offset, nbytes):
@@ -35,7 +39,9 @@ def get_moisture():
     return ret
 
 
-def tick(timer):
+def main_tick(timer):
+    # update_safe_time(localtime(), get_safe_time())
+    
     m = get_moisture()
     ledred.value(m < 925)
     c_m = int((m - 300) / 7)
@@ -44,24 +50,53 @@ def tick(timer):
     ledgreen.value(t > 28)
     c_t = int(t - 5)
     write_value(m)
-    print(f"Fuktighet: {c_m}%, Temp {c_t} C")
+    print(f"Fuktighet: {m}%, Temp {t} C")
 
 
 def write_value(x):
-    file1 = open(DATA_FILENAME, "a")
+    file1 = open(DATA_FILE, "a")
     l = localtime()
     file1.write(f"{x} ({l[2]}/{l[1]}/{l[0]} {l[3]}:{l[4]})\n")
     file1.close()
 
 
+def get_safe_time():
+    time_file = open(TIME_FILE, "r")
+    r = time_file.readline()
+    time_file.close()
+    return eval(r)
+
+
+def use_safe_time(t):
+    rtc = RTC()
+    rtc.datetime(t)
+
+
+def update_safe_time(t, s):
+    if t[0] >= s[0] and t[7] >= s[7]:
+        time_file = open(TIME_FILE, "w")
+        time_file.write(str(t))
+        time_file.close()
+    
+
 # Setup
+use_safe_time(START_TIME)
+"""
+use_safe_time(START_TIME)
+if localtime()[0] < get_safe_time()[0]:
+    s = get_safe_time()
+    print(s)
+    use_safe_time(s)
+else:
+    update_safe_time(localtime(), get_safe_time())
+"""
 ledred = Pin(10, Pin.OUT)
 ledgreen = Pin(11, Pin.OUT)
 
 i2c = I2C(0, scl=machine.Pin(17), sda=machine.Pin(16))
 
 tim = Timer()
-tim.init(period=MEASUREMENT_DELAY, callback=tick)
+tim.init(period=MEASUREMENT_DELAY, callback=main_tick)
 
 
 # Program start
