5768f3a94b7e323e308aa9323fe2afbb122a0a39
axel-nils-chalmers <axelnils@student.chalmers.se>
2023-01-14 16:38:34 +0100

    minor plotting changes

--- plot.py
+++ plot.py
@@ -4,28 +4,45 @@ For plotting data
 
 from is_data import DataDict
 import pandas as pd
-import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 
 DATA_FILE = "data/data.json"
 
-d = DataDict(DATA_FILE).data
-keys = sorted(d.keys())
-y = [d[key]["moisture"] for key in keys]
-x = [pd.to_datetime(key) for key in keys]
 
-plt.title("Moisture data")
-plt.xlabel("date and time")
-plt.ylabel("moisture level")
+def get_data():
+    d = DataDict(DATA_FILE).data
+    keys = sorted(d.keys())
+    y = [d[key]["moisture"] for key in keys]
+    x = [pd.to_datetime(key) for key in keys]
+    return x, y
 
-ax = plt.gca()
-formatter = mdates.DateFormatter("%Y-%m-%d %H:%M")
-ax.xaxis.set_major_formatter(formatter)
-locator = mdates.AutoDateLocator()
-ax.xaxis.set_major_locator(locator)
 
-plt.plot(x, y)
-plt.ylim(0, 1.5*max(y))
-plt.grid()
-plt.show()
+def set_labels():
+    plt.title("Moisture data")
+    plt.xlabel("date and time")
+    plt.ylabel("moisture level")
+
+
+def apply_formatting():
+    ax = plt.gca()
+
+    locator = mdates.AutoDateLocator()
+    ax.xaxis.set_major_locator(locator)
+    # formatter = mdates.DateFormatter("%Y-%m-%d %H:%M")
+    formatter = mdates.AutoDateFormatter(locator)
+    ax.xaxis.set_major_formatter(formatter)
+
+    min_y, max_y = min(y), max(y)
+    y_range = max_y - min_y
+    plt.ylim(min_y - y_range, max_y + y_range)
+
+
+if __name__ == "__main__":
+    x, y = get_data()
+    plt.plot(x, y)
+    set_labels()
+    apply_formatting()
+    plt.grid()
+    plt.show()
+
