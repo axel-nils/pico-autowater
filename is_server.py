428fa4593435caf0aa7d618f663c7f9c53bb353d
axel-nils-chalmers <axelnils@student.chalmers.se>
2023-03-04 13:52:10 +0100

    separated http requests for html and css

--- is_server.py
+++ is_server.py
@@ -12,17 +12,24 @@ from my_credentials import WIFI_NAME, WIFI_PSW
 TIME_API_URL = "https://www.timeapi.io/api/Time/current/zone?timeZone=Europe/Stockholm"
 LED_ONBOARD = machine.Pin("LED", machine.Pin.OUT)
 TEMP_SENSOR = machine.ADC(4)
-CONVERSION_FACTOR = 3.3 / (65535)
+CONVERSION_FACTOR = 3.3 / 65535
 
 
 class DataServer:
-    def __init__(self, max_wait=10, html_file="index.html"):
+    def __init__(self, max_wait=10, html_file="index.html", css_file="style.css"):
         self.max_wait = max_wait
         self.html_file = html_file
+        self.css_file = css_file
+
+        with open(self.css_file, 'r') as file:
+            self.css = str(file.read())
+
         self.ip = None
         self.port = 80
         self.host = "0.0.0.0"
+
         self.connect()
+
         self.state = "OFF"
         try:
             asyncio.run(self.run())
@@ -46,16 +53,15 @@ class DataServer:
             raise RuntimeError("Network connection failed")
         else:
             self.ip = wlan.ifconfig()[0]
-            print(f"Connected to {WIFI_NAME} with IP-adress {self.ip}")
+            print(f"Connected to {WIFI_NAME} with IP-address {self.ip}")
 
     async def serve(self, reader, writer):
         request_line = await reader.readline()
         while await reader.readline() != b"\r\n":
             pass
 
-        self.handle_request(request_line)
-        response = self.create_response()
-        writer.write('HTTP/1.0 200 OK\r\nContent-type: text/html\r\n\r\n')
+        header, response = self.handle_request(request_line)
+        writer.write(header)
         writer.write(response)
         await writer.drain()
         await writer.wait_closed()
@@ -67,15 +73,30 @@ class DataServer:
         except IndexError:
             pass
 
+        header = 'HTTP/1.1 404 Not Found\r\nContent-Type: text/css\r\n\r\n'
+        response = ""  # TODO not found page
+
+        print(request)
+        html_requests = ["/", "/index.html", "/lighton?", "/lightoff?"]
+        if request in html_requests:
+            print("assuming html response wanted")
+            header = 'HTTP/1.1 200 OK\r\nContent-type: text/html\r\n\r\n'
+            response = self.create_html_response()
+        elif request == "/style.css":
+            print("responding with css")
+            header = 'HTTP/1.1 200 OK\r\nContent-Type: text/css\r\n\r\n'
+            response = self.css
+        
         if request == "/lighton?":
             LED_ONBOARD.on()
-            self.state = 'ON'
+            self.state = "ON"
         elif request == "/lightoff?":
             LED_ONBOARD.off()
-            self.state = 'OFF'
+            self.state = "OFF"
 
+        return header, response
 
-    def create_response(self):
+    def create_html_response(self):
         temperature = get_temp()
         time_now = get_time()
 
@@ -92,7 +113,6 @@ class DataServer:
         asyncio.create_task(asyncio.start_server(self.serve, self.host, self.port))
         while True:
             await asyncio.sleep(1)
-
 
 
 def get_datetime_json():
@@ -124,4 +144,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
