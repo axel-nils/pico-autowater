87c11770dcf22731e62d0306099303e20925227e
axel-nils <axelnilsson.onsala@live.se>
2023-03-11 16:42:22 +0100

    wifi debug

--- utils/wifi.py
+++ utils/wifi.py
@@ -8,20 +8,26 @@ class WiFi:
     def __init__(self, ssid, psw):
         self.ip = None
 
-        wlan = network.WLAN(network.STA_IF)
-        wlan.active(True)
-        wlan.config(pm=0xa11140)  # Disable power saver-mode
-        wlan.connect(ssid, psw)
+        self.wlan = network.WLAN(network.STA_IF)
+        self.wlan.active(True)
+        self.wlan.config(pm=0xa11140)  # Disable power saver-mode
+        self.wlan.connect(ssid, psw)
 
         while self.MAX_WAIT > 0:
-            if wlan.status() < 0 or wlan.status() >= 3:
+            if self.wlan.status() < 0 or self.wlan.status() >= 3:
                 break
             self.MAX_WAIT -= 1
             print("Waiting for wifi connection...")
             sleep(1)
 
-        if wlan.status() != 3:
+        if self.wlan.status() != 3:
             raise RuntimeError("Wifi connection failed")
         else:
-            self.ip = wlan.ifconfig()[0]
+            self.ip = self.wlan.ifconfig()[0]
             print(f"Connected to {ssid} with IP-address {self.ip}")
+
+
+if __name__ == "__main__":
+    from utils import WIFI_NAME, WIFI_PASS
+    wifi = WiFi(WIFI_NAME, WIFI_PASS)
+    print(wifi.wlan.ifconfig())
