96026bab93ef26e7c2301bc53c52e1c2f2966ae7
axel-nils <axelnilsson.onsala@live.se>
2023-04-01 21:43:22 +0200

    charts overhaul, moisture gauge added

--- web/app.js
+++ web/app.js
@@ -1,53 +1,70 @@
-google.charts.load('current', {packages: ['corechart'], 'language': 'sv'}).then(drawCharts);
+google.charts.load("current", {packages: ["corechart", "gauge"], "language": "sv"}).then(drawCharts);
 window.onload = (event) => {
   getWeatherJson(weatherUrl)
 };
 
-const em = parseInt(window.getComputedStyle(document.getElementById('download_btn')).fontSize);
-const color = '#243B10';
-const white = '#F8F8F8';
-const weatherUrl = 'https://api.openweathermap.org/data/2.5/weather?id=6943587&units=metric&lang=se&appid=87d1bfdb974592195531cdf4aae52fd2';
+const em = parseInt(window.getComputedStyle(document.getElementById("download_btn")).fontSize);
+const gaugeSize = parseInt(window.getComputedStyle(document.getElementById("on_btn")).width);
+const green = "#243B10";
+const red = "3B1210";
+const white = "#F8F8F8";
+const weatherUrl = "https://api.openweathermap.org/data/2.5/weather?id=6943587&units=metric&lang=se&appid=87d1bfdb974592195531cdf4aae52fd2";
 
-const chartOptions = {
-  chartArea: {width: '100%', top: 0, bottom: 2*em},
-  backgroundColor: color,
-  colors: [white],
-  fontSize: em,
-  fontName: 'Raleway',
-  vAxis: {
-    gridlines: {count: 0},
-    textStyle: {color: white},
-    textPosition: 'in'
-  },
-  hAxis: {
-    format: 'EEE',
-    gridlines: {color: white, count: 6},
-    textStyle: {color: white},
-  }
-};
+function getIntFromElementId(id) {
+  const maybeInt = parseInt(document.getElementById(id).textContent);
+  return isNaN(maybeInt) ? 0 : maybeInt;
+}
 
 async function getWeatherJson(url) {
   const response = await fetch(url);
   const json = await response.json();
-  const desc = json.weather[0].description;
+  const description = json.weather[0].description;
   const temp = parseInt(json.main.temp);
-  document.getElementById("weather").innerHTML = temp + ' °C och ' + desc;
+  document.getElementById("weather").innerHTML = temp + " °C och " + description;
 }
 
 async function getDataJson(filePath) {
   const response = await fetch(filePath);
-  const json = await response.json();
-  return json;
+  return await response.json();
 }
 
+const lineChartOptions = {
+  chartArea: {width: "90%"},
+  backgroundColor: green,
+  colors: [white],
+  fontSize: em,
+  fontName: "Raleway",
+  titleTextStyle: {
+    color: white, fontName: "Marcellus", bold: false
+  },
+  vAxis: {
+    gridlines: {count: 0},
+    textStyle: {color: white},
+    textPosition: "in"
+  },
+  hAxis: {
+    format: "EEE",
+    gridlines: {color: white},
+    minorGridlines: {count: 0},
+    textStyle: {color: white},
+  },
+  legend: {position: "none"}
+};
+
+
+const gaugeOptions = {
+  width: 0.9 * gaugeSize, height: 0.9 * gaugeSize,
+  redFrom: 0, redTo: getIntFromElementId("dry_string"),
+  greenColor: green, greenFrom: getIntFromElementId("wet_string"), greenTo: 100,
+};
+
 function getLineChart(chartId) {
-  const chart = new google.visualization.LineChart(document.getElementById(chartId));
-  return chart;
+  return new google.visualization.LineChart(document.getElementById(chartId));
 }
 
 function getDataTable(data_array, colNr, type, name) {
   const dt = new google.visualization.DataTable();
-  dt.addColumn('datetime', 'X');
+  dt.addColumn("datetime", "X");
   dt.addColumn(type, name);
 
   const arr = [];
@@ -56,25 +73,31 @@ function getDataTable(data_array, colNr, type, name) {
   }
   dt.addRows(arr);
 
-  const fmt_d = new google.visualization.DateFormat({pattern: 'EEEE d MMM HH:mm'});
+  const fmt_d = new google.visualization.DateFormat({pattern: "EEEE d MMM HH:mm"});
   fmt_d.format(dt, 0);
 
   return dt
 }
 
 async function drawCharts() {
-  const data_json = await getDataJson('data.json')
+  const data_json = await getDataJson("data.json")
   const data_array = data_json.data;
 
-  const m_chart = getLineChart('m_chart_div');
-  const m_data = getDataTable(data_array, 1, 'number', 'Jordfuktighet');
-  const fmt_m = new google.visualization.NumberFormat({pattern:'##.#', suffix: '%'});
-  fmt_m.format(m_data, 1);
-  m_chart.draw(m_data, chartOptions);
+  const g_chart =new google.visualization.Gauge(document.getElementById("gauge_div"));
+  const g_data = google.visualization.arrayToDataTable([
+    ["Label", "Value"],
+    ["Fuktighet", getIntFromElementId("moisture_string")]]);
+  g_chart.draw(g_data, gaugeOptions);
 
-  const t_chart = getLineChart('t_chart_div');
-  const t_data = getDataTable(data_array, 2, 'number', 'Lufttemperatur');
-  const fmt_t = new google.visualization.NumberFormat({pattern: '##.#°C'});
+  const m_chart = getLineChart("m_chart_div");
+  const m_data = getDataTable(data_array, 1, "number", "Jordfuktighet");
+  const fmt_m = new google.visualization.NumberFormat({pattern:"##.#", suffix: "%"});
+  fmt_m.format(m_data, 1);
+  m_chart.draw(m_data, {...lineChartOptions, title: "Jordfuktighet [%]"});
+
+  const t_chart = getLineChart("t_chart_div");
+  const t_data = getDataTable(data_array, 2, "number", "Lufttemperatur");
+  const fmt_t = new google.visualization.NumberFormat({pattern: "##.#°C"});
   fmt_t.format(t_data, 1);
-  t_chart.draw(t_data, chartOptions);
+  t_chart.draw(t_data, {...lineChartOptions, title: "Lufttemperatur [°C]"});
 }
