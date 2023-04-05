c81f7d2879d0974336d5db8ff30846286e163a0d
axel-nils <axelnilsson.onsala@live.se>
2023-04-05 19:35:57 +0200

    water valve tested working

--- devices/pin_config.py
+++ devices/pin_config.py
@@ -4,7 +4,7 @@ LED_GRN = Pin(11, Pin.OUT)
 LED_RED = Pin(10, Pin.OUT)
 LED_ONBOARD = Pin("LED", Pin.OUT)
 
+PIN_VALVE = Pin(28, Pin.OUT)
+
 PIN_SCL = Pin(17)
 PIN_SDA = Pin(16)
-
-PIN_VALVE = Pin(28, Pin.OUT)  # TODO: Check if this is correct
--- devices/water_valve.py
+++ devices/water_valve.py
@@ -8,14 +8,13 @@ from machine import Pin
 class WaterValve:
     def __init__(self, pin: Pin):
         self.pin = pin
-        self.is_open = False
 
     def open(self):
-        if not self.is_open:
-            self.is_open = True
-            self.pin.on()
+        self.pin.on()
 
     def close(self):
-        if self.is_open:
-            self.is_open = False
-            self.pin.off()
+        self.pin.off()
+
+    @property
+    def is_open(self):
+        return bool(self.pin.value())
